a2efad225754b6162cb2dc4a37341acc5b6cefed
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-19 06:59:18 +0100

    Modified the student.rb file to fix linters error

--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
 require_relative 'person'
 
-class student < person
-  def initialize (@classroom, age, name ='unknown', parent_permission: true)
+class Student < person
+  def initialize (classroom, age, name ='Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
-  
+
   def play_hooky
     "¯\(ツ)/¯"
   end
