6d18ee484a1795e8395b8395850b58c5a9ee1bb7
Hammed Adisa <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-20 06:53:49 +0100

    Added base Decorator which inherits from Nameable

--- nameable.rb
+++ nameable.rb
@@ -3,3 +3,16 @@ class Nameable
     raise 'NotImplementedError'
   end
 end
+
+class Decorator < Nameable
+  attr_accessor: nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
