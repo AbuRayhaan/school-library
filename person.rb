cf54ad5540ebaeee28e4b974eaf09d4dcf3aea31
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-22 20:11:51 +0100

    Modified person.rb to order the parameters

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :books, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -33,6 +33,6 @@ class Person < Nameable
 end
 
 
-# Tobi = Person.new(5,'Gra')
+# tobi = Person.new(5,'Gra', 'y')
 
-# print Tobi
+# print tobi
