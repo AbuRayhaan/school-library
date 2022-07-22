040d0ab3938d14228d580253835033136cb97e99
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-22 07:33:35 +0100

    Modified the main.rb file

--- main.rb
+++ main.rb
@@ -2,35 +2,7 @@ require_relative app
 
 def main
   app = App.new()
-  app.run()
+  app.start
 end
 
-def list_options
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
-
-def option(input)
-  case input
-  when '1'
-    'List all books'
-  when '2'
-    'List all people'
-  when '3'
-    'Create a person'
-  when '4'
-    'Create a book'
-  when '5'
-    'Create a rental'
-  when '6'
-    'List all rentals'
-  else
-    'Kindly enter a number between 1 and 7'
-end
-main()
+main
