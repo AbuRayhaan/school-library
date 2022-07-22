27158d3b83326815235c041d7012029992fdd47f
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-22 17:53:30 +0100

    Modified app.rb file

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-require_relative book
-require_relative person
-require_relative student
-require_relative teacher
-require_relative rental
+require_relative 'book'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
 
 class App
   def initialize()
@@ -108,36 +108,37 @@ class App
       puts 'Records not found for given ID'
     end
   end
-
-  def execute_inputs
-    input = Integer(gets.chomp)
-    case input
-    when 1 then list_books
-    when 2 then list_people
-    when 3 then create_person
-    when 4 then create_book
-    when 5 then create_rental
-    when 6 then list_all_rentals
-    when 7
-      puts 'Thank you for using this app. Goodbye'
-      exit
-    else
-      puts 'Kindly enter a number between 1-7'
+  
+def execute_inputs
+  input = Integer(gets.chomp)
+  case input
+  when 1 then list_books
+  when 2 then list_people
+  when 3 then create_person
+  when 4 then create_book
+  when 5 then create_rental
+  when 6 then list_all_rentals
+  when 7
+    puts 'Thank you for using this app. Goodbye'
+    exit
+  else
+    puts 'Kindly enter a number between 1-7'
   end
+end
 
-  def start
-    puts 'Welcome to School Library App!'
-    puts ' '
-    puts 'Please choose an option by entering a number:'
-    loop do
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      execute_inputs
-    end
+def start
+  puts 'Welcome to School Library App!'
+  puts ' '
+  puts 'Please choose an option by entering a number:'
+  loop do
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    execute_inputs
   end
+end
 end
