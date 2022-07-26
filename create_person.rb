081ba63ce5dfa54e0d5cba553f252961ef781704
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-26 15:49:59 +0100

    Created class CreatePerson

---
+++ create_person.rb
@@ -0,0 +1,20 @@
+class CreatePerson
+  def initialize(people)
+    @people = people
+    @create_teacher = CreateTeacher.new(people)
+    @create_student = CreateStudent.new(people)
+  end
+
+  def create_person
+    puts 'To create a student press (1), to create a teacher press (2): '
+    n = gets.chomp
+    case n
+    when '1'
+      @create_student.create_student
+    when '2'
+      @create_teacher.create_teacher
+    else
+      puts 'Invalid selection'
+    end
+  end
+end
