4a8c3f942842b59e5f073052333d6a54a8cf1b59
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-28 18:52:45 +0100

    Restructure the code

---
+++ core/student.rb
@@ -0,0 +1,19 @@
+require_relative 'person'
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self)
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
