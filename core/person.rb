0770c5c2551c3ec7e874e2e8e3432a317b3f0716
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-29 10:48:51 +0100

    Modified person file

--- core/person.rb
+++ core/person.rb
@@ -2,7 +2,7 @@ require './switches/nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :books, :rentals
+  attr_accessor :name, :age, :books, :rentals, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
