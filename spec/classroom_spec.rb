a59ec0045051c6b6ade3cc7b8c66ed481c8bc303
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-29 13:49:10 +0100

    Tested the Classroom class

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,15 @@
+require './core/classroom'
+require './core/student'
+
+describe do
+  context 'Classroom class' do
+    before (:each) do
+      @chemistry_class = Classroom.new 'Chemistry classroom'
+      @biology_class = Classroom.new 'Biology classroom'
+    end
+    it 'should display classroom label' do
+      expect(@chemistry_class.label).to eq 'Chemistry classroom'
+      expect(@biology_class.label).to eq 'Biology classroom'
+    end
+  end
+end
